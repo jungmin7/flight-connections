cf13b85ed3105b81692eebeb9660409b72f491a3
Jay Jo <jrjo2@siebl-0224-22.ews.illinois.edu>
2020-12-05 21:54:20 -0600

    edited haversine function

--- project_files/main.cpp
+++ project_files/main.cpp
@@ -9,15 +9,14 @@ using namespace std;
 #include <utility> // for pair
 #include <cmath>
 // #define pi 3.14159265358979323846
-#define earthRadiusKm 6371.0
 
-    class testing {
+    class testingHaversine {
         public:
             // double toRad(double degree);
             // double calculateDistance(double lat1, double long1, double lat2, double long2);
 
-            double deg2rad(double deg);
-            double rad2deg(double rad);
+            double degTorad(double deg);
+            double radTodeg(double rad);
 
             double distanceEarth(double lat1d, double lon1d, double lat2d, double lon2d);
 
@@ -48,31 +47,37 @@ double radToDeg(double rad) {
 
 /**
  * Returns the distance between two points on the Earth using Haversine Formula.
- * @param lat1d Latitude of the first point in degrees
- * @param lon1d Longitude of the first point in degrees
- * @param lat2d Latitude of the second point in degrees
- * @param lon2d Longitude of the second point in degrees
- * @return The distance between the two points in kilometers
+ * https://www.movable-type.co.uk/scripts/latlong.html
+ * Parameters all in degrees.
+ * Returns distance between two points in kilometers.
  */
 double distanceEarth(double lat1d, double lon1d, double lat2d, double lon2d) {
-  double lat1r, lon1r, lat2r, lon2r, u, v;
-  lat1r = degToRad(lat1d);
-  lon1r = degToRad(lon1d);
-  lat2r = degToRad(lat2d);
-  lon2r = degToRad(lon2d);
-  u = sin((lat2r - lat1r)/2);
-  v = sin((lon2r - lon1r)/2);
-  return 2.0 * earthRadiusKm * asin(sqrt(u * u + cos(lat1r) * cos(lat2r) * v * v));
+
+  double lat1_rad = degToRad(lat1d);
+  double lon1_rad = degToRad(lon1d);
+  double lat2_rad = degToRad(lat2d);
+  double lon2_rad = degToRad(lon2d);
+
+  double latDiff = (lat2_rad - lat1_rad);
+  double lonDiff = (lon2_rad - lon1_rad);
+  
+  double a = sin(latDiff / 2) * sin(latDiff / 2)   +   cos(lat1_rad) * cos(lat1_rad) * sin(lonDiff / 2) * sin(lonDiff / 2);
+  double earthRadius = 6371.0; //in KILOMETERS. https://en.wikipedia.org/wiki/Earth
+  return 2 * earthRadius * asin(sqrt(a));
+
+  // radius varies on Earth: 6356.752 km at the poles to 6378.137 km at the equator. 
+  // computing SIN to ICN results in 4780.65
 }
 
 
 int main()
 {   
-    /*
+    /**
     Flights air;
     air.readData("routes_test.txt");
-    return 0;
+    //return 0;
     */
+    
 
 
     Graph g1 = Graph(true, true);
@@ -95,6 +100,8 @@ int main()
 
     cout << SINtoICNDistance << endl;
 
+    // g1.print();
+
 
 
    // TRASH CODE:
@@ -110,6 +117,41 @@ int main()
 
     //ex: "SIN", pair<10.40, 20.20>
     //    "ICN", pair<5.4, 5.0>
+
+
+
+
+    Graph g2 = Graph(true, true);
+    g2.insertEdge("SIN", "ICN");
+    g2.insertEdge("ICN", "NRT");
+    g2.insertEdge("ICN", "JFK");
+    g2.insertEdge("JFK", "NRT");
+    // g2.insertEdge("NRT", "JFK"); //testing two paths to same vertices.
+    g2.setEdgeWeight("SIN", "ICN", 10); 
+    g2.setEdgeWeight("ICN", "NRT", 30);
+    g2.setEdgeWeight("ICN", "JFK", 20);
+    g2.setEdgeWeight("JFK", "NRT", 40);
+    // g2.setEdgeWeight("NRT", "JFK", 40);
+
+    // g2.print();
+    /**
+    class testingBFS {
+
+    };
+
+    int numVertices = getVertices().size(); //method that returns # of vertices
+
+    bool * visited = new bool[numVertices];
+    for (int i = 0; i < vertices; i++) {
+        visited[i] = false;
+    }
+
+    //queue
+    list<double> queue;
+
+    visited[s] = true;
+    */
+
 
 }
 
