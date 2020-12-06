4f4f7b88b4fcad9293a525d6e4a2eeff3aff41e2
Jay Jo <jrjo2@linux-a2.ews.illinois.edu>
2020-12-06 05:04:09 -0600

    readAirports works, but not when our data files have spaces in lines

--- project_files/airport.cpp
+++ project_files/airport.cpp
@@ -35,8 +35,77 @@ Flights::Flights() : g(true, true) {
  * @return vector<string>
  */
 map<string, pair<double, double>> Flights::readAirports(const string &filename) {
-    map<string, pair<double, double>> myMap;
-    return myMap;
+    // map<string, pair<double, double>> myMap;
+    // return myMap;
+
+    ifstream text(filename);
+	vector<string> out;
+    
+    map<string, pair<double, double>> coordinateMap; //mapping airport to <longitude, latitude>
+
+	if (text.is_open()) {
+		istream_iterator<string> iter(text);
+		while (!text.eof()) {
+			out.push_back(*iter);
+			++iter;
+		}
+	}
+
+    for(unsigned i = 0; i < out.size(); i++) { //traverses line by line.
+        string str = out[i];
+        int count = 0;
+        int fourthCommaIndex;
+        int sixthCommaIndex;
+        int seventhCommaIndex;
+        int eighthCommaIndex;
+
+        cout << str << endl;
+
+        for (unsigned j = 0; j < str.length(); j++) { //traverses each line to find indexes of commas.
+            if(str.at(j) == ',' && count <= 8) { //up until 8th comma because information after that is irrelevant.
+                count++;
+                if (count == 4 || count == 6 || count == 7 || count == 8) {
+                    // firstAirport = str.substr(j+2, 3); //ex: ,"GKA",
+                    //cout << firstAirport << " ";
+                    if (count == 4) {
+                        fourthCommaIndex = j;
+                    } else if (count == 6) {
+                        sixthCommaIndex = j;
+                    } else if (count == 7) {
+                        seventhCommaIndex = j;
+                    } else if (count == 8) {
+                        eighthCommaIndex = j;
+                    }
+                }
+                /**
+                if (count == 4 || count == 6 || ) {
+                    secondAirport = str.substr(j+1, 3);
+                    cout << secondAirport << " " << endl;
+                }
+                */
+                
+            }
+        }
+
+        cout << fourthCommaIndex << " " <<sixthCommaIndex << " " << seventhCommaIndex << endl;
+        //cout << "\n" << out[i] << endl;
+
+        //finding the airport name(ex: "SIN"), longitude string and latitude string.
+            string airport = str.substr(fourthCommaIndex+2, 3); //ex: ,"GKA",
+            string latitude_str = str.substr(sixthCommaIndex+1, (seventhCommaIndex-sixthCommaIndex-1)); //ex: ,-6.081689834590001, 
+            string longitude_str = str.substr(seventhCommaIndex+1, (eighthCommaIndex-seventhCommaIndex-1)); 
+
+            //convert strings to doubles; works for negative too.
+            double latitude = stringToDouble(latitude_str);
+            double longitude = stringToDouble(longitude_str);
+
+            cout << airport << " " << latitude << " " << longitude << endl;
+            
+            //enter inside map (ex: myMap["SIN"] = pair<double, double>(1.35019, 103.994003);)
+            coordinateMap[airport] = pair<double, double>(latitude, longitude);
+        
+    }
+	return coordinateMap; //return the map of strings to pairs.
 }
 
 
