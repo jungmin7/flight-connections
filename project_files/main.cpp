d099254e1a3cac8dd199a7ae0f50bf08f6a211c3
Jay Jo <jrjo2@linux-a2.ews.illinois.edu>
2020-12-05 18:19:00 -0600

    jay test branch

--- project_files/main.cpp
+++ project_files/main.cpp
@@ -2,11 +2,36 @@
 #include "airport.h"
 #include <iostream>
 
+#include "edge.h"
+
+#include <map>
+
+#include <utility> // for pair
+
 using namespace std;
 
 int main()
-{
+{   
+    /*
     Flights air;
     air.readData("routes_test.txt");
     return 0;
+    */
+
+
+    Graph g1 = Graph(true, true);
+    g1.insertEdge("SIN", "ICN");
+    g1.insertEdge("ICN", "JFK");
+    g1.setEdgeWeight("SIN", "ICN", 10); 
+    g1.setEdgeWeight("ICN", "JFK", 20);
+
+    //ex: "SIN", pair<10.40, 20.20>
+    //    "ICN", pair<5.4, 5.0>
+    map<string, pair<double, double>> map1;
+    map1.insert(pair<string, pair<double, double>>("SIN", )
+
+    )
+
+    pair<double, double>(10.40, 20.2);
+
 }
