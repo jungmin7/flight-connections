e208299a07fc0e92652d64256b416597e684abd2
Hemank Kohli <hkohli2@linux-a2.ews.illinois.edu>
2020-12-11 03:10:00 -0600

    created templated main.cpp DO NOT CHANGE

--- project_files/main.cpp
+++ project_files/main.cpp
@@ -12,7 +12,43 @@ using namespace std;
 
 int main()
 {
-    Flights air("routes_test.txt", "airports.txt");
+    string routes = "routes.txt"; //replace text file name here if need be but format needs to be the same as used in OpenFlights routes.dat file
+    string airports = "airports.txt"; //replace text file name here if need be but format needs to be the same as used in OpenFlights airports.dat file
+
+    //intializing object
+    Flights air(routes, airports);
+
+    /*
+
+    //start of BFS
+    cout << " " << endl;
+    string start = "SIN"; //replace with the desired 3-letter airport code for traversal
+    air.BFS(start);
+    //end of bfs
+
+    */
+
+    /*
+
+    //start of shortest path
+    cout << " " << endl;
+    string source = "SIN"; //replace with 3-letter code for the desired source airport
+    string destination = "ICN"; //replace with 3-letter code for the desired destination airport
+    int dist = air.shortestPath(source, destination);
+    cout << "The distance between " << source <<  " " << "and " << destination << " is " << dist << " km" << endl;
+    //end of shortest path
+
+    */
+
+    /*
+
+    //start of strongly connected components
+    //prints strongly connected component based on data files given from constructor
+    cout << " " << endl;
+    air.stronglyConnected();
+    //end of strongly connected components
+
+    */
 
     return 0;
 
