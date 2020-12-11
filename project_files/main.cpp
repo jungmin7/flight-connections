1d1ce048ad3724131f3a53da4deea5d8df16ffce
Hemank Kohli <hkohli2@linux-a2.ews.illinois.edu>
2020-12-11 11:39:51 -0600

    refer to README.md for more instructions

--- project_files/main.cpp
+++ project_files/main.cpp
@@ -15,6 +15,10 @@ int main()
     string routes = "routes.txt"; //replace text file name here if need be but format needs to be the same as used in OpenFlights routes.dat file
     string airports = "airports.txt"; //replace text file name here if need be but format needs to be the same as used in OpenFlights airports.dat file
 
+    //uncomment the next two lines to run main under subset of the OpenFlights data set
+    //string routes = "routes_test.txt";
+    //string airports = "airports_test.txt";
+
     //intializing object
     Flights air(routes, airports);
 
@@ -22,7 +26,7 @@ int main()
 
     //start of BFS
     cout << " " << endl;
-    string start = "SIN"; //replace with the desired 3-letter airport code for traversal
+    string start = "KZN"; //replace with the desired 3-letter airport code for traversal
     air.BFS(start);
     //end of bfs
 
@@ -32,8 +36,8 @@ int main()
 
     //start of shortest path
     cout << " " << endl;
-    string source = "SIN"; //replace with 3-letter code for the desired source airport
-    string destination = "ICN"; //replace with 3-letter code for the desired destination airport
+    string source = "KZN"; //replace with 3-letter code for the desired source airport
+    string destination = "DME"; //replace with 3-letter code for the desired destination airport
     int dist = air.shortestPath(source, destination);
     cout << "The distance between " << source <<  " " << "and " << destination << " is " << dist << " km" << endl;
     //end of shortest path
